91c96d5a8a7e9f8169c54f931b5733faa0a9aa75
Pradeep Kumar <kumarpradeep1010@gmail.com>
2018-10-25 00:00:48 -0500

    feat: adding seo meta tag and code refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'; 
 
@@ -29,7 +28,6 @@ import { NewsService } from './services/news.service';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot(reducers),
